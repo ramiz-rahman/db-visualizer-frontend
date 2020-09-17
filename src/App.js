0604e22bf72f5e11a95da70fbead1e269c0cb02f
ramiz-rahman <ramiz.rahman@outlook.com>
2020-09-18 01:12:15 +0600

    Initial Commit: Setup Table and API Connection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Table from './components/Table';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    error: null,
+    phones: [],
+    specs: [],
+  };
+
+  componentDidMount() {
+    const api = 'http://localhost:3000';
+    const headers = {
+      Accept: 'application/json',
+    };
+
+    fetch(`${api}/phones`, { headers })
+      .then((res) => res.json())
+      .then(
+        (data) => this.setState({ phones: data }),
+        (error) => this.setState({ error })
+      );
+
+    fetch(`${api}/specs`, { headers })
+      .then((res) => res.json())
+      .then(
+        (data) => this.setState({ specs: data }),
+        (error) => this.setState({ error })
+      );
+  }
+
+  render() {
+    const { phones, specs } = this.state;
+    const phoneHeaders =
+      phones.length > 0 ? Object.keys(phones[0]) : null;
+    const specHeaders = specs.length > 0 ? Object.keys(specs[0]) : null;
+    return (
+      <div className="App">
+        <div>
+          <h2>Phones</h2>
+          {phoneHeaders ? (
+            <Table headers={phoneHeaders} items={phones} />
+          ) : null}
+        </div>
+        <div>
+          <h2>Specs</h2>
+          {specHeaders ? (
+            <Table headers={specHeaders} items={specs} />
+          ) : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Table/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './style.css';
+
+const Table = ({ headers, items }) => {
+  return (
+    <table className="Table">
+      <thead>
+        <tr className="Table__Header">
+          {headers.map((header) => (
+            <th className="Table__Cell">{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item) => (
+          <tr>
+            {Object.keys(item).map((key) => (
+              <td className="Table__Cell">{item[key]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
